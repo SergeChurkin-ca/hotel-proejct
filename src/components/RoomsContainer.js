c71c44dd6ea171704524c2d52dce1d6fe0c1caea
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-10-21 12:52:07 -0400

    higher order component

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,29 +1,41 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter'
 import RoomList from './RoomList'
-import { RoomConsumer } from '../context';
+import { withRoomConsumer } from '../context';
 import Loading from './Loading';
 
-export default function RoomsContainer() {
-    return (
-        <RoomConsumer>
-            {
-                (value) => {
-                    const { loading, sortedRooms, rooms } = value
-                    if (loading) {
-                        return (
-                                <Loading/>
-                            )
-                        }
-                    return (
-                        <div>
-                            Hello from rooms container
-                            <RoomsFilter rooms={rooms}/>
-                            <RoomList rooms={sortedRooms}/>
-                        </div>
-                    )
-                }
-            }
-        </RoomConsumer>
-    )
+function RoomContainer({ context }) {
+    
 }
+
+
+export default withRoomConsumer(RoomContainer)
+// import React from 'react'
+// import RoomsFilter from './RoomsFilter'
+// import RoomList from './RoomList'
+// import { RoomConsumer } from '../context';
+// import Loading from './Loading';
+
+// export default function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//             {
+//                 (value) => {
+//                     const { loading, sortedRooms, rooms } = value
+//                     if (loading) {
+//                         return (
+//                                 <Loading/>
+//                             )
+//                         }
+//                     return (
+//                         <div>
+//                             Hello from rooms container
+//                             <RoomsFilter rooms={rooms}/>
+//                             <RoomList rooms={sortedRooms}/>
+//                         </div>
+//                     )
+//                 }
+//             }
+//         </RoomConsumer>
+//     )
+// }
