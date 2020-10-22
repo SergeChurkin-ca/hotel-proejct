6509746e7254b730073a337d7572ddc5a0a6a2da
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-10-21 23:50:03 -0400

    filter rooms

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title  from '../components/Title';
 
-export default function RoomsFilter() {
+// get unque values to put to dropdown menu
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))];
+}
+
+export default function RoomsFilter({rooms}) {
+    const context = useContext(RoomContext);
+    const { handleChange, type, capacity, price, minPRice, maxPrice, minSize, maxSize, breakfast, pets } = context;
+
+    let types = getUnique(rooms, 'type');
+    types = ['all',...type];
+    // map to jsx
+    types = types.map((item, index) => {
+        return <option value={item} key={index}>{item}</option>
+    })
+
     return (
-        <div>
-            hello from room filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>{types}</select>
+                </div>
+                {/* end select type */}
+            </form>
+        </section>
     )
 }
