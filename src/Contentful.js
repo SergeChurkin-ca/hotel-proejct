b17e0f064bf656e0e5a24771221d46f312400212
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-11-18 13:32:05 -0500

    setup contentful API

---
+++ src/Contentful.js
@@ -0,0 +1,7 @@
+import { createClient } from 'contentful'
+
+export default createClient({
+    space: "z247qyv3mji3",
+    accessToken: "paoOmiCdyDi8_6YGBBY9PjWGtErlwv6BEUngRjY8c9w",
+
+});
--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import items from './data'
+import Client from './Contentful'
 
 const RoomContext = React.createContext();
 
@@ -21,8 +22,13 @@ class RoomProvider extends Component {
     };
     //  getData
 
-    componentDidMount() {
-        let rooms = this.formateData(items);
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "hotelRoom"
+            });
+            
+  let rooms = this.formateData(response.items);
         let featuredRooms = rooms.filter(room => room.featured === true);
         let maxPrice = Math.max(...rooms.map(item => item.price));
         let maxSize = Math.max(...rooms.map(item=> item.size))
@@ -35,6 +41,14 @@ class RoomProvider extends Component {
             maxPrice,
             maxSize
         })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    componentDidMount() {
+      this.getData()
     }
 
     formateData(items) {
